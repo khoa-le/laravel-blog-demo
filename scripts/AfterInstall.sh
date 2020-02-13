447e68a5f800fea8741a360728d9de5fef9b3232
Khoa <khoaln6@gmail.com>
2020-02-13 18:37:00 +0700

    Update after install

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -9,8 +9,9 @@ cd /var/www/html
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php -- --install-dir=/home/ec2-user --filename=composer
 sudo rm composer-setup.php
+sudo mv /home/ec2-user/composer /usr/bin/composer
 
-sudo /usr/bin/composer.phar install --no-ansi --no-dev --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/html
+sudo /usr/bin/composer install --no-ansi --no-dev --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/html
 #
 # Run artisan commands
 php /var/www/html/artisan migrate
